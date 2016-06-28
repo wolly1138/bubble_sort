2fffd44d3159df5089c83c9077bb3c1f3b18cbf7
wolly1138 <wolly1138@sohu.com>
2016-06-28 14:57:31 +0000

    初始文件

---
+++ readme.txt
@@ -0,0 +1,5 @@
+编译
+gcc -o test test.c
+测试
+./test a b c d e f
+./test i am a student
---
+++ test.c
@@ -0,0 +1,53 @@
+#include "stdio.h"
+#include "string.h"
+
+#define MAX_WORD_NUM		32
+
+#define DONE		0		//已经完成排序
+#define UNDONE		1		//未完成排序
+
+int main(int argc, char** argv)
+{
+	int i,j;
+	int flag = UNDONE;		//监控是否已经有序
+
+	char *temp;
+	char *buf[MAX_WORD_NUM];
+	int buf_len = argc - 1;
+	
+	int cnt = 0;				//记录外循环次数
+
+	if(argc < 2)
+		return -1;
+
+	for(i=0; i<buf_len; i++)
+		buf[i] = argv[i+1];
+
+	/*冒泡排序，注意循环的层次与方向*/
+	for(i=1; i<buf_len && flag; i++)
+	{
+		flag = DONE;
+		cnt++;
+
+		for(j=buf_len-1; j>=i; j--)
+		{
+			if(strcmp(buf[j-1],buf[j]) > 0)	//从小向大排列
+			{
+				temp = buf[j];
+				buf[j] = buf[j-1];
+				buf[j-1] = temp;
+				
+				flag = UNDONE;
+			}
+		}
+	}
+
+	for(i=0; i<buf_len-1; i++)
+		printf("%s ",buf[i]);
+
+	printf("%s\n",buf[buf_len-1]);
+
+	printf("sort finished in %d cycles\n",cnt);
+
+	return 0;
+}
